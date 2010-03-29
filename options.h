72d7fe031bb59b549ab6e081aa20d4c0e254988c
wrog <>
2010-03-29 21:59:49 +0000

    Set better MAX_CONCAT_* limits; revised description

--- options.h
+++ options.h
@@ -312,24 +312,27 @@
 /* #define USE_GNU_MALLOC */
 
 /******************************************************************************
- * DEFAULT_MAX_LIST_CONCAT, if set to a postive value, is the length of the
- * largest list constructible via splicing and subrange assignment.
- * This is overriden by $server_options.max_list_concat if that is defined.
- * DEFAULT_MAX_STRING_CONCAT, if set to a postive value, is the length of the
- * largest string constructible via concatenation and subrange assignment
- * This is overriden by $server_options.max_string_concat if that is defined.
- * In either case, specifying a zero or negative limit disables this checking.
- * 
+ * DEFAULT_MAX_LIST_CONCAT,   if set to a postive value, is the length
+ *                            of the largest constructible list.  
+ * DEFAULT_MAX_STRING_CONCAT, if set to a postive value, is the length
+ *                            of the largest constructible string.
+ * Limits on "constructible" values apply to values built by concatenation,
+ * splicing, subrange assignment and various builtin functions.
+ * If defined in the database, $server_options.max_list_concat and
+ * and $server_options.max_string_concat override these defaults.
+ * A zero value disables limit checking.  A positive value that is
+ * less than the minimum specified in server.h is boosted accordingly.
+ *
  * $server_options.max_concat_catchable, if defined, causes an E_QUOTA error
  * to be raised when an overly-large value is spotted.  Otherwise, the task
- * is aborted as if it ran out of seconds (see DEFAULT_FG/BG_SECONDS),
- * which was the original behavior in this situation (i.e., if we were lucky
- * enough to avoid the server panicking due to memory allocation failure).
+ * is aborted as if it ran out of seconds (see DEFAULT_FG_SECONDS),
+ * which was the original behavior in this situation (i.e., if we
+ * were lucky enough to avoid a server memory allocation panic).
  ******************************************************************************
  */
 
-#define DEFAULT_MAX_LIST_CONCAT   16777216
-#define DEFAULT_MAX_STRING_CONCAT 16777216
+#define DEFAULT_MAX_LIST_CONCAT    4194302
+#define DEFAULT_MAX_STRING_CONCAT 33554423
 
 
 /*****************************************************************************
@@ -423,6 +426,10 @@
 
 /* 
  * $Log$
+ * Revision 1.13  2010/03/27 00:10:33  wrog
+ * IGNORE_PROP_PROTECTED now off by default and deprecated;
+ * Undo accidental uncommenting of OUTBOUND_NETWORK
+ *
  * Revision 1.12  2010/03/26 23:51:42  wrog
  * New server options max_*_concat and max_concat_catchable
  *
